d78926a6f2831c78e0f5a60d53f664d4c0a8c43a
BM <sid89004@gmail.com>
2026-03-19 17:35:37 +0100

    zmiana na ten basic tekst

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,17 @@
 import 'package:flutter/material.dart';
-
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-
-
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("KrakFlow"),
+        ),
+        body: Text("Hello"),
+      ),
     );
   }
 }
-
