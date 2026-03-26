4cfe630d431e43e4e2c35953282deffbb3b41a00
Bartosz Migacz <s175337@student.uken.krakow.pl>
2026-03-26 17:50:25 +0100

    do zad 5

--- lib/main.dart
+++ lib/main.dart
@@ -3,23 +3,99 @@ void main() {
   runApp(MyApp());
 }
 class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  final List<Task> tasks = [
+    Task(title: "zrobic projekt strony", deadline: "31.03.2026"),
+    Task(title: "fluter", deadline: "26.03.2026 18:00"),
+    Task(title: "Aisd", deadline: "30.03.2026"),
+  ];
+
+
+
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
       home: Scaffold(
         appBar: AppBar(
           title: Text("KrakFlow"),
         ),
-        body: Column(
+
+
+        body: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text("Krakflow "),
-              Text('Organizacja studiow '),
-              Text('Dzisiejsze zadania'),
+              const Text(
+                "Dzisiejsze zadania",
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 16),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: tasks.length,
+                  itemBuilder: (context, index){
+                    final task = tasks[index];
+
+                    return TaskCard(
+                      title: task.title,
+                      subtitle: "termin: ${task.deadline}",
+                      icon: Icons.radio_button_unchecked,
+                    );
+                  },
+                ),
+              ),
             ],
+          ),
         ),
       ),
-
     );
   }
 }
+
+class Task {
+  final String title;
+  final String deadline;
+  Task({
+    required this.title,
+    required this.deadline
+  });
+}
+
+class TaskCard extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final IconData icon;
+
+  const TaskCard({
+    super.key,
+    required this.title,
+    required this.subtitle,
+    required this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.only(bottom: 12),
+      child: Padding(
+        padding: const EdgeInsets.all(8),
+        child: ListTile(
+          leading: Icon(icon),
+          title: Text(title),
+          subtitle: Text(subtitle),
+        ),
+      ),
+      );
+  }
+}
+
+
+
+
