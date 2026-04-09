3eec202347fd8b0877fddfba230a9ee04f198282
bartosz migacz <s175337@student.uken.krakow.pl>
2026-04-09 17:52:16 +0200

    Lab 5 do zadania 5

--- lib/main.dart
+++ lib/main.dart
@@ -3,66 +3,78 @@ void main() {
   runApp(MyApp());
 }
 class MyApp extends StatelessWidget {
-  MyApp({super.key});
-
-  final List<Task> tasks = [
-    Task(title: "zrobic projekt strony", deadline: "31.03.2026", done: true, priority: "wysoki"),
-    Task(title: "fluter", deadline: "26.03.2026 18:00", done: true, priority: "sredni"),
-    Task(title: "Aisd", deadline: "30.03.2026", done: false, priority: "niski"),
-  ];
-
-
-
-
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("KrakFlow"),
-        ),
+      home: const HomeScreen(),
+    );
+  }
+}
 
 
-        body: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "masz dzis ${tasks.length} zadania",
-                style: const TextStyle(
-                  fontSize: 22,
-                  fontWeight: FontWeight.bold,
-                ),
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    int completedCount = TaskRepository().tasks.where((t) => t.done).length;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("KrakFlow"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Masz dziś ${TaskRepository().tasks.length} zadania",
+              style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            ),
+            Text("Wykonano: $completedCount"),
+            const SizedBox(height: 16),
+            const Text(
+              "Dzisiejsze zadania",
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+            ),
+            const SizedBox(height: 16),
+            Expanded(
+              child: ListView.builder(
+                itemCount: TaskRepository().tasks.length,
+                itemBuilder: (context, index) {
+                  return TaskCard(task: TaskRepository().tasks[index]);
+                },
               ),
-              const SizedBox(height: 16),
-              Expanded(
-                child: ListView.builder(
-                  itemCount: tasks.length,
-                  itemBuilder: (context, index){
-                    final task = tasks[index];
-
-                    return TaskCard(task: task);
-                  },
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
+      ),
 
-        floatingActionButton: FloatingActionButton(onPressed: (){
-
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const AddTaskScreen(),
+            ),
+          );
         },
         child: const Icon(Icons.add),
-        ),
-
-
-
       ),
     );
   }
 }
+
 
 class Task {
   final String title;
@@ -101,6 +113,30 @@ class TaskCard extends StatelessWidget {
             subtitle: Text("termin: ${task.deadline} | priorytet: ${task.priority}"),
           ),
         );
+  }
+}
+
+class TaskRepository{
+  final List<Task> tasks = [
+    Task(title: "zrobic projekt strony", deadline: "31.03.2026", done: true, priority: "wysoki"),
+    Task(title: "fluter", deadline: "26.03.2026 18:00", done: true, priority: "sredni"),
+    Task(title: "Aisd", deadline: "30.03.2026", done: false, priority: "niski"),
+  ];
+}
+
+class AddTaskScreen extends StatelessWidget {
+  const AddTaskScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Nowe zadanie"),
+      ),
+      body: const Center(
+        child: Text("Tutaj będzie formularz dodawania taska"),
+      ),
+    );
   }
 }
 
